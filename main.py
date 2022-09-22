dea22bc5baca628e7a18621512cd26dc1e7d8b71
AgentYman <114188508+AgentYman@users.noreply.github.com>
2022-09-23 05:12:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ meet_date = os.environ['MEET_DATE']
 love_date = os.environ['LOVE_DATE']
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + str(city)
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
