9a4af256d38a531c44ad9fdc8cd261c1065658c0
AgentYman <114188508+AgentYman@users.noreply.github.com>
2022-09-23 06:06:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import os
 import random
 
 today = datetime.now()
+meet_date = os.environ['MEET_DATE']
+love_date = os.environ['LOVE_DATE']
 
 city = os.environ['CITY']
 # birthday = os.environ['BIRTHDAY']
@@ -17,14 +19,16 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-meet_date = os.environ['MEET_DATE']
-love_date = os.environ['LOVE_DATE']
+def get_today():
+    week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+    week = week_list[today.isoweekday()]
+    return str(today).split()[0] + " " + week
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + str(city)
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['temp']), math.floor(weather['tempn'])  # temp is highest ; tempn is lowest
 
 def get_meetdays():
   delta = today - datetime.strptime(meet_date, "%Y-%m-%d")
@@ -53,7 +57,13 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weathre":{"value":wea},"temperature":{"value":temperature},"meet_days":{"value":get_meetdays()},"love_days":{"value":get_lovedays()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, temperature, temperature_n = get_weather()
+data = {"today":{"value":get_today(), "color":get_random_color()},
+        "weather":{"value":wea, "color":get_random_color()},
+        "temperature":{"value":temperature, "color":get_random_color()},
+        "temperaturen":{"value":temperature_n, "color":get_random_color()},
+        "meet_days":{"value":get_meetdays(), "color":get_random_color()},
+        "love_days":{"value":get_lovedays(), "color":get_random_color()},
+        "words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
